3095f00ca8c5a7920a3728677acb2fced23d30ae
deepgt <pisces981111@gmail.com>
2022-04-20 13:58:55 +0545

    1.0.0

---
+++ main.py
@@ -0,0 +1,132 @@
+
+import math
+import cv2 as cv
+import numpy as np
+from torch import sort
+
+#loading image 
+img = cv.imread('test01.jpg')
+# cv.imshow('test',img)
+
+#since there are 4 sperate rectangle i want to divide the image into four parts 
+#first vertically 
+cv.imshow('original', img)
+
+imginverted = cv.bitwise_not(img)
+
+
+height = imginverted.shape[0]
+width = imginverted.shape[1]
+
+# Cut the image in half
+width_cutoff = width // 2
+left1 = imginverted[:, :width_cutoff]
+right1 = imginverted[:, width_cutoff:]
+
+# finish vertical devide image
+
+#rotate image LEFT1 to 90 CLOCKWISE
+leftimg = cv.rotate(left1, cv.ROTATE_90_CLOCKWISE)
+
+# start vertical devide image
+height = leftimg.shape[0]
+width = leftimg.shape[1]
+# Cut the image in half
+width_cutoff = width // 2
+l1 = leftimg[:, :width_cutoff]
+l2 = leftimg[:, width_cutoff:]
+# finish vertical devide image
+
+#rotate image to 90 COUNTERCLOCKWISE
+l1 = cv.rotate(l1, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+#rotate image to 90 COUNTERCLOCKWISE
+l2 = cv.rotate(l2, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+#rotate image LEFT1 to 90 CLOCKWISE
+rightimg = cv.rotate(right1, cv.ROTATE_90_CLOCKWISE)
+
+# start vertical devide image
+height = rightimg.shape[0]
+width = rightimg.shape[1]
+# Cut the image in half
+width_cutoff = width // 2
+r1 = rightimg[:, :width_cutoff]
+r2 = rightimg[:, width_cutoff:]
+# finish vertical devide image
+
+#rotate image to 90 COUNTERCLOCKWISE
+r1 = cv.rotate(r1, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+#rotate image to 90 COUNTERCLOCKWISE
+r2 = cv.rotate(r2, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated1 = rotate(l1, -30)
+rotated2= rotate(l2, 15)
+rotated3 = rotate(r1, 30)
+rotated4 = rotate(r2, -15)
+
+h_img1 = cv.hconcat([rotated2, rotated4])
+h_img2 = cv.hconcat([rotated1, rotated3])
+v_img = cv.vconcat([h_img1,h_img2])
+
+# cv.imshow('final image',cv.bitwise_not(v_img))
+
+# Edge detection
+dst = cv.Canny(cv.bitwise_not(v_img), 50, 200, None, 3)
+# cv.imshow('dst',dst)
+
+linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 10, None, 10, 5)
+
+# print(linesP)
+
+# if linesP is not None:
+#     for i in range(0, len(linesP)):
+#         l = linesP[i][0]
+#         cv.line(v_img, (l[0], l[1]), (l[2], l[3]), (0,255,0), 1)
+
+# 4. Draw a line
+# cv.line(v_img, (104,77),(190,77), (0,255,0), thickness=1)
+# cv.line(v_img, (489,63),(620,63), (0,255,0), thickness=1)
+# cv.line(v_img, (89,378) ,(265,378), (0,255,0), thickness=1)
+# cv.line(v_img, (464,458),(547,458), (0,255,0), thickness=1)
+
+#x1,y1 and x2,y2
+l1=[104,77,190,77]
+l2=[489,63,620,63]
+l3=[89,378,265,378]
+l4=[464,458,547,458]
+
+line1 = l1[2]-l1[0]
+line2 = l2[2]-l2[0]
+line3 = l3[2]-l3[0]
+line4 = l4[2]-l4[0]
+
+lengths = [line1,line2,line3,line4]
+
+print(lengths)
+
+# sort_lengths = sorted(lengths.items(), key=lambda x: x[1], reverse= True)
+
+textpositionx = [150,550,150,550] 
+textpositiony = [180,180,300,300] 
+i=0
+
+for length in lengths :
+    cv.putText(v_img, f"{ lengths[i] }" , (textpositionx[i],textpositiony[i]), cv.QT_FONT_NORMAL, 1.0, (255,255,255), 1)    
+    i=i+1
+cv.imshow('lines', cv.bitwise_not(v_img))
+
+cv.waitKey(0)
